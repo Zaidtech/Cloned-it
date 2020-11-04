50a296422ae4f0dd90f6145c3efcebd1276c74b1
Zaidtech <mzali@zhcet.ac.in>
2020-11-04 15:20:36 +0530

    Delete saved pages

--- application.py
+++ application.py
@@ -26,37 +26,47 @@ def index():
 @app.route("/main" ,methods=["POST"])
 def main():
     fox = webdriver.Firefox(executable_path=PATH)
-    url = request.form.get("url")
-    fox.get(url)
+    try:
+        url = request.form.get("url")
+        fox.get(url)
+    except:
+	    pass
     # try to find a way to hide the above open window
     time.sleep(10)
     while True:
-        page = fox.page_source
-        with open(f"./templates/cloned0.html", 'w') as f:
-            f.write(page)
+        try:
+            page = fox.page_source
+            with open(f"./templates/cloned0.html", 'w') as f:
+                f.write(page)
+        except:
+            print("Couldn't find something")        
         # the above page is the source of html 
         # parsing thos page into the bs4obj
         # print(page)
         random.seed(datetime.datetime.now())
+        time.sleep(1)
         internalLinks = []
         externalLinks = []
         
         #Retrieves a list of all Internal links found on a page
         def getInternalLinks(bsObj, includeUrl):
-            random.seed(datetime.datetime.now())    
+            time.sleep(0.2)    
         #Finds all links that begin with a "/" ie the internal links 
             for link in bsObj.find_all("a", href=re.compile("^(/|.*"+includeUrl+")")):
                 if link.attrs['href'] is not None:
                     i = 0
                     # if link.attrs['href'] not in internalLinks:
                     internalLinks.append(link.attrs['href'])
-                    fox.get(link.attrs['href'])
-                    url = link.attrs['href']
-                    page = fox.page_source
-                    time.sleep(5)
-                    with open(f"./templates/cloned_internal{i}.html", 'w') as f:
-                        f.write(page)
-                    i= i+1
+                    try:
+                        fox.get(link.attrs['href'])
+                        
+                        page = fox.page_source
+                        time.sleep(5)
+                        with open(f"./templates/cloned_internal{i}.html", 'w') as f:
+                            f.write(page)
+                        i= i+1
+                    except:
+                        print("NOt allowed")   
             return internalLinks
         
         #Retrieves a list of all external links found on a page
@@ -70,8 +80,6 @@ def main():
                     #     # print(f"Adding {link.attrs['href']}  to externalLinks")
                     # externalLinks.append(link.attrs["href"])
                     fox.get(link.attrs['href'])
-                    
-                    url = link.attrs['href']
                     page = fox.page_source
                     time.sleep(5)
                     with open(f"./templates/cloned_external{j}.html", 'w') as f:
@@ -89,18 +97,21 @@ def main():
         
         def getLinks(startingPage):
             # parsinng and creating objects of page
-            html = urlopen(startingPage)
-            bsObj = BeautifulSoup(html, 'html.parser')
-            # searching for external links on this page
-            externalLinks = getExternalLinks(bsObj, splitAddress(startingPage)[0])
-            if len(externalLinks) == 0:
-                internalLinks = getInternalLinks(bsObj, startingPage)
-                print("An internal link")
-                try:
-                    return internalLinks[random.randint(0, len(externalLinks)-1)]
-                except:
-                    return externalLinks[0]
+            try:
+                html = urlopen(startingPage)
+                bsObj = BeautifulSoup(html, 'html.parser')
+                # searching for external links on this page
+                externalLinks = getExternalLinks(bsObj, splitAddress(startingPage)[0])
+                if len(externalLinks) == 0:
                     
+                    internalLinks = getInternalLinks(bsObj, startingPage)
+                    print("An internal link")
+                    try:
+                        return internalLinks[random.randint(0, len(externalLinks)-1)]
+                    except:
+                        return externalLinks[0]
+            except:
+                pass       
                     
             else:
                 print("found an external link")
@@ -110,33 +121,31 @@ def main():
                     externalLinks[0]    
         
         def followExternalOnly(startingSite):
-            link_internal_external = getLinks(startingSite) 
-            html = urlopen(link_internal_external)
-            bsObj = BeautifulSoup(html, 'html.parser')
-            tags = bsObj.find_all('div')
-            # imgs = bsObj.find_all('img')
-        #    specifically for watsAPP  
-        # imgs = bsObj.find_all("img",{"class":"_2goTk _1Jdop _3Whw5"})
-            for tag in tags:
-                print(link_internal_external)
-                # print(tag.text)
-                # print("\n \n \n ")
-            # try:
-            #     for img in imgs:
-            #         imgUrl = img['src']
-            #         print(imgUrl)
-            #         print("downloading ....")
-            #         name = random.randrange(1,1000)
-            #         fullname = str(name)+".jpg"
-            #         urllib.request.urlretrieve(imgUrl,fullname)     
-        
-            # except:
-            #     pass
+            link_internal_external = getLinks(startingSite)
+            try: 
+                html = urlopen(link_internal_external)
+                bsObj = BeautifulSoup(html, 'html.parser')
+                # tags = bsObj.find_all('div')
+                imgs = bsObj.find_all('img')
+            except:
+                print(f"Acess DEnied on !! {link_internal_external}")
+            try:
+                for img in imgs:
+                    imgUrl = img['src']
+                    print(imgUrl)
+                    print("downloading ....")
+                    time.sleep(3)
+                    name = random.randrange(1,1000)
+                    fullname = str(name)+".jpg"
+                    urllib.request.urlretrieve(imgUrl,fullname)     
+            except:
+                pass
             # print(link_internal_external)
             try:
                 followExternalOnly(link_internal_external)
             except:
                 print("Unable to acess")
+                time.sleep(5)
                 followExternalOnly(externalLinks[0])
         followExternalOnly(url)
         # url = input("Enter the base Url to start with : ")
